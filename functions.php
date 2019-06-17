393799fecdcca51298ac8238cacf36b63285e662
Julio Cruz <atlascruz009@gmail.com>
2019-06-17 11:05:02 -0700

    added bootstrap

--- functions.php
+++ functions.php
@@ -123,9 +123,16 @@ add_action( 'widgets_init', 'plumtor_widgets_init' );
 function plumtor_scripts() {
 	wp_enqueue_style( 'plumtor-style', get_stylesheet_uri() );
 
+	// Bootstrap CSS
+	wp_enqueue_style( 'plumtor-bootstrap', get_stylesheet_uri() . '/css/bootstrap.min.js' );
+	
+
 	wp_enqueue_script( 'plumtor-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
 	wp_enqueue_script( 'plumtor-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+
+	// Boostrap JS
+	wp_enqueue_script( 'plumtor-bootstrap-js', get_template_directory_uri() . '/js/bootstrap.min.js', array(), 20151215, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
