7e7ff79323223f37dcc9114e6731b812cbd4021f
Julio Cruz <atlascruz009@gmail.com>
2019-06-20 17:52:23 -0700

    Styles input and button

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -11,42 +11,46 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		<div class="container">
+			<?php the_title('<h1 class="entry-title">', '</h1>'); ?>
+		</div>
 	</header><!-- .entry-header -->
+	<div class="container page-wrapper">
+		<?php plumtor_post_thumbnail(); ?>
 
-	<?php plumtor_post_thumbnail(); ?>
-
-	<div class="entry-content">
-		<?php
-		the_content();
-
-		wp_link_pages( array(
-			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'plumtor' ),
-			'after'  => '</div>',
-		) );
-		?>
-	</div><!-- .entry-content -->
-
-	<?php if ( get_edit_post_link() ) : ?>
-		<footer class="entry-footer">
+		<div class="entry-content">
 			<?php
-			edit_post_link(
-				sprintf(
-					wp_kses(
-						/* translators: %s: Name of current post. Only visible to screen readers */
-						__( 'Edit <span class="screen-reader-text">%s</span>', 'plumtor' ),
-						array(
-							'span' => array(
-								'class' => array(),
-							),
-						)
-					),
-					get_the_title()
-				),
-				'<span class="edit-link">',
-				'</span>'
-			);
+			the_content();
+
+			wp_link_pages(array(
+				'before' => '<div class="page-links">' . esc_html__('Pages:', 'plumtor'),
+				'after'  => '</div>',
+			));
 			?>
-		</footer><!-- .entry-footer -->
-	<?php endif; ?>
+		</div><!-- .entry-content -->
+		<?php if (get_edit_post_link()) : ?>
+			<footer class="entry-footer">
+				<?php
+				edit_post_link(
+					sprintf(
+						wp_kses(
+							/* translators: %s: Name of current post. Only visible to screen readers */
+							__('Edit <span class="screen-reader-text">%s</span>', 'plumtor'),
+							array(
+								'span' => array(
+									'class' => array(),
+								),
+							)
+						),
+						get_the_title()
+					),
+					'<span class="edit-link">',
+					'</span>'
+				);
+				?>
+			</footer><!-- .entry-footer -->
+		<?php endif; ?>
+	</div> <!-- .container -->
+
+
 </article><!-- #post-<?php the_ID(); ?> -->
